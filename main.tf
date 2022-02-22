ecd55a4f8c4aba9ee435ffd22e66cd6074e52fe3
Patrick <pcarey@hashicorp.com>
2022-02-22 16:53:39 +1100

    test echo var

--- main.tf
+++ main.tf
@@ -146,12 +146,17 @@ resource "aws_instance" "hashicat" {
 # Set up some environment variables for our script.
 # Add execute permissions to our scripts.
 # Run the deploy_app.sh script.
+
 resource "null_resource" "configure-cat-app" {
   depends_on = [aws_eip_association.hashicat]
 
   triggers = {
     build_number = timestamp()
   }
+
+  provisioner "local-exec" {
+    command = "echo ${priv_key}"
+    }
 
   provisioner "file" {
     source      = "files/"
