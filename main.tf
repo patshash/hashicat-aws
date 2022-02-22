5171417636010dbd75fa259853a181598868578e
Patrick <pcarey@hashicorp.com>
2022-02-22 17:10:00 +1100

    remove agent check

--- main.tf
+++ main.tf
@@ -188,6 +188,7 @@ resource "null_resource" "configure-cat-app" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
+      agent       = false
       #private_key = tls_private_key.hashicat.private_key_pem
       private_key = "${var.priv_key}"
       host        = aws_eip.hashicat.public_ip
