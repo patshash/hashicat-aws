5e1788be8be0bdf7e992c1d36d29786f997a289a
Patrick <pcarey@hashicorp.com>
2022-02-22 14:14:23 +1100

    update

--- main.tf
+++ main.tf
@@ -182,7 +182,8 @@ resource "null_resource" "configure-cat-app" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = tls_private_key.hashicat.private_key_pem
+      #private_key = tls_private_key.hashicat.private_key_pem
+      private_key = var.priv_key
       host        = aws_eip.hashicat.public_ip
     }
   }
