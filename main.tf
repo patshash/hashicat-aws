03c59a50842a8235b586d6c2bdc2bfc291549827
Patrick <pcarey@hashicorp.com>
2022-03-16 15:50:11 +1100

    tidy up

--- main.tf
+++ main.tf
@@ -189,8 +189,7 @@ resource "null_resource" "configure-cat-app" {
       type        = "ssh"
       user        = "ubuntu"
       agent       = false
-      #private_key = tls_private_key.hashicat.private_key_pem
-      private_key = "${var.priv_key}"
+      private_key = tls_private_key.hashicat.private_key_pem
       host        = aws_eip.hashicat.public_ip
     }
   }
