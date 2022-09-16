1eccefb736025b62631a07a5ec244fe52d1ae8dc
Patrick <82187582+patshash@users.noreply.github.com>
2022-09-16 16:07:06 +1000

    remove upgrade and cowsay

--- main.tf
+++ main.tf
@@ -173,15 +173,11 @@ resource "null_resource" "configure-cat-app" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt -y update",
-      "sleep 15",
-      "sudo apt -y update",
       "sudo apt -y install apache2",
       "sudo systemctl start apache2",
       "sudo chown -R ubuntu:ubuntu /var/www/html",
       "chmod +x *.sh",
       "PLACEHOLDER=${var.placeholder} WIDTH=${var.width} HEIGHT=${var.height} PREFIX=${var.prefix} ./deploy_app.sh",
-      "sudo apt -y install cowsay",
-      "cowsay Mooooooooooo!",
     ]
 
     connection {
